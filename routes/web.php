bb5782521ba2bcc9235dba7da5cceb8f3f9fd2aa
salhia <moheha2@gmail.com>
2025-04-19 00:34:36 +0400

    update

--- routes/web.php
+++ routes/web.php
@@ -332,7 +332,7 @@ Route::post('/agent/details/message', [IndexController::class, 'AgentDetailsMess
 Route::get('/rent/property', [IndexController::class, 'RentProperty'])->name('rent.property');
 
 //Get all Buy property
-Route::get('/buy/property', [IndexController::class, 'BuyProperty'])->name('buy.property');
+//Route::get('/buy/property', [IndexController::class, 'BuyProperty'])->name('buy.property');
 
 //Get Property type details
 Route::get('/property/type/{id}', [IndexController::class, 'PropertyType'])->name('property.type');
